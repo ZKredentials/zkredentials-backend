a94df3df363f05a011c9d20f767619b3ef98230e
gob <bennettlikebeckham@gmail.com>
2023-04-15 16:46:55 +0900

    feat: cid

--- app/server.js
+++ app/server.js
@@ -6,16 +6,43 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import { gql, GraphQLClient } from 'graphql-request'
 import { generateProof } from "./zk-snarks.js";
+import { Web3Storage, getFilesFromPath, File } from 'web3.storage'
+
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
-
+const WEB3STORAGE_TOKEN = process.env.WEB3STORAGE_TOKEN;
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 const GITHUB_REDIRECT_URI =process.env.GITHUB_CALLBACK;
 const EXAMPLE_REPO_OWNER = "Multinate";
 const EXAMPLE_REPO = "web3-donation-aggregator";
+
+function makeStorageClient () {
+  return new Web3Storage({ token: WEB3STORAGE_TOKEN })
+}
+function makeFileObjects (obj, name) {
+  // You can create File objects from a Buffer of binary data
+  // see: https://nodejs.org/api/buffer.html
+  // Here we're just storing a JSON object, but you can store images,
+  // audio, or whatever you want!
+  // const obj = { hello: 'world' }
+  const buffer = Buffer.from(JSON.stringify(obj))
+
+  const files = [
+    new File([buffer], `${name}.json`)
+  ]
+  return files
+}
+
+async function storeFiles (files) {
+  const client = makeStorageClient()
+  const cid = await client.put(files)
+  console.log('stored files with cid:', cid)
+  return cid
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
@@ -70,12 +97,30 @@ app.post("/generate", async (req, res) => {
     const sponsorProof = await generateProof(sponsors, sponsorsThreshold);
     const starredProof = await generateProof(starred, starredThreshold);
     const prsProof = await generateProof(prs, prsThreshold);
+    const sponsorProofFile = await makeFileObjects({
+    sponsorProof: sponsorProof.proof
+    }, 'sponsorProof')
+    const sponsorProofFileCID = await storeFiles(sponsorProofFile)
+    const starredProofFile = await makeFileObjects({
+    starredProof: starredProof.proof
+    }, 'starredProof')
+    const starredProofFileCID = await storeFiles(starredProofFile)
+    const prsProofFile = await makeFileObjects({
+    prsProof: prsProof.proof
+    }, 'prsProof')
+    const prsProofFileCID = await storeFiles(prsProofFile)
+
+    const resumeFile = await makeFileObjects({
+      prs: prsProofFileCID,
+      starred: starredProofFileCID,
+      sponsor: sponsorProofFileCID
+      }, 'resume')
+    const resumeCID = await storeFiles(resumeFile)
     res.send({
-      sponsorProof: sponsorProof.proof,
-      starredProof: starredProof.proof,
-      prsProof: prsProof.proof
+      cid: resumeCID
     });
   } catch (error) {
+    console.log(error)
     res.send({
       e: error,
     });
